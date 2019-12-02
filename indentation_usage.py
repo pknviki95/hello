6846887e18ecd4b9c7fcd0dbc81f82d682211fd1
pknviki95 <vikidotcom95@gmail.com>
2019-12-02 14:00:05 +0530

    indentation_usage.py script

---
+++ indentation_usage.py
@@ -0,0 +1,6 @@
+print("This is for indentation usage")
+if (1 < 3):
+	print("The condition is true")
+	print("the indent is for conditions")
+for i in range(0,2):
+	print("this indent is for functions")
