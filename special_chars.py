ddcac986e5807ff6dc77773815bf2d747de83f9f
pknviki95 <vikidotcom95@gmail.com>
2019-12-02 14:00:46 +0530

    special_chars.py script

---
+++ special_chars.py
@@ -0,0 +1,26 @@
+print("This is for special chars")
+
+
+#\n
+print("This new tab is used to print in new line")
+print("Hello\nWorld")
+
+#\b
+print("This back space tab is used to get back cursor in one space")
+print("Hello \bWorld")
+
+
+#\t
+print("This gives tab space")
+print("Hello \tWorld")
+
+
+#'',""(\-escape symbol)
+
+print('This is their\'s')
+# different quotes used to eliminate this error
+print("different quotes used to eliminate this error")
+print("this is their's")
+
+#unicode Error
+print("C:\\home\\pknviki95\\Desktop\\python")
