113f507ef3b29a0d0be5e37e2ceeef5fdbfd1f68
vigneshwaran M <vikidotcom95@gmail.com>
2020-01-31 19:16:17 +0530

    git operations using python scripts

---
+++ Python_program_practice/git_python_scripts/git_python.py
@@ -0,0 +1,11 @@
+import git
+repo = git.Repo( '/home/pknviki95/Desktop/Python' )
+print (repo.git.status())
+# checkout and track a remote branch
+#print repo.git.checkout( 'origin/somebranch', b='somebranch' )
+#add a file
+print (repo.git.add( '.' ))
+#commit
+print (repo.git.commit( m='git operations using python scripts' ))
+#now we are one commit ahead
+print (repo.git.status())
